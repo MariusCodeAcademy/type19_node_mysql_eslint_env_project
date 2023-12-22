88ef0cbdd8cd5248c7bdc7ce6f2f276e254e5ec4
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-22 08:59:38 +0200

    test conn

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const dbConfig = {
-  host: process.env.DB_USER,
-  user: process.env.DB_PASS,
-  password: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
 };
 
-console.log('dbConfig ===', dbConfig);
+// console.log('dbConfig ===', dbConfig);
 
 module.exports = {
   dbConfig,
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
+const { dbConfig } = require('./config');
 
 const app = express();
 
@@ -16,6 +17,22 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.json('Hello World');
 });
+
+testConnection();
+// connect
+async function testConnection() {
+  let conn;
+  try {
+    conn = await mysql.createConnection(dbConfig);
+    await conn.query('SELECT * FROM posts LIMIT 1');
+    console.log('Succesfuly connected to mysql');
+  } catch (error) {
+    console.log('testConnection failed, did you start XAMPP mate???');
+    console.log(error);
+  } finally {
+    if (conn) conn.end();
+  }
+}
 
 app.listen(PORT, () => {
   console.log(`Server runing on http://localhost:${PORT}`);
